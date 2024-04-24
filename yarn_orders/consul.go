7e33a6c5f9f8f0d24b0f37e4fd59ffb062ed8a92
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-04-24 16:20:01 -0600

    init yarn_orders

---
+++ yarn_orders/consul.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	consulapi "github.com/hashicorp/consul/api"
+)
+
+func Register() {
+	config := consulapi.DefaultConfig()
+	config.Address = "consul:8500"
+	consul, err := consulapi.NewClient(config)
+	if err != nil {
+		fmt.Println(err)
+	}
+	serviceId := "go-orders-service:" + getHostname()
+	port, _ := strconv.Atoi(getPort()[1:len(getPort())])
+	fmt.Printf("port:%v \n", port)
+	address := getHostname()
+	fmt.Printf("address:%v \n", address)
+	tags := []string{
+		"traefik.enable=true",
+		fmt.Sprintf("traefik.http.routers.%s.rule=PathPrefix(`/orders`)", getHostname()),
+	}
+	registeration := &consulapi.AgentServiceRegistration{
+		Tags:    tags,
+		ID:      serviceId,
+		Name:    "catalog",
+		Port:    port,
+		Address: address,
+		Check: &consulapi.AgentServiceCheck{
+			HTTP:     fmt.Sprintf("http://%s:%v/orders/check", address, port),
+			Interval: "10s",
+			Timeout:  "30s",
+		},
+	}
+
+	regiErr := consul.Agent().ServiceRegister(registeration)
+	if regiErr != nil {
+		log.Panic(regiErr)
+		log.Printf("Failed to register service: %s:%v ", address, port)
+	} else {
+		log.Printf("successfully register service: %s:%v", address, port)
+	}
+}
+
+func getPort() (port string) {
+	port = os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	port = ":" + port
+	return
+}
+
+func getHostname() (hostname string) {
+	hostname, _ = os.Hostname()
+	return
+}
