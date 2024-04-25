bdf537112048e72ce7afbb889e944e3ab92f47b4
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-04-25 09:43:16 -0600

    update

--- yarn_email/main.go
+++ yarn_email/main.go
@@ -47,71 +47,9 @@ func main() {
 	go func() {
 		for d := range msgs {
 			log.Printf("Sending email confirmation to: %s", d.Body)
-			SendUserEmail(string(d.Body))
 		}
 	}()
 
 	log.Print("Waiting for messages")
 	<-forever
 }
-
-// package main
-
-// import (
-// 	"log"
-
-// 	"github.com/streadway/amqp"
-// )
-
-// func main() {
-// 	Register()
-// 	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 	}
-// 	defer conn.Close()
-
-// 	ch, err := conn.Channel()
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 	}
-// 	defer ch.Close()
-
-// 	q, err := ch.QueueDeclare(
-// 		"order", // name
-// 		false,   // durable
-// 		false,   // delete when unused
-// 		false,   // exclusive
-// 		false,   // no-wait
-// 		nil,     // arguments
-// 	)
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 	}
-
-// 	msgs, err := ch.Consume(
-// 		q.Name, // queue
-// 		"",     // consumer
-// 		true,   // auto-ack
-// 		false,  // exclusive
-// 		false,  // no-local
-// 		false,  // no-wait
-// 		nil,    // args
-// 	)
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 	}
-
-// 	var forever chan struct{}
-
-// 	go func() {
-// 		for d := range msgs {
-// 			log.Printf("Sending email confirmation to: %s", d.Body)
-// 			// send email
-// 			SendUserEmail(string(d.Body))
-// 		}
-// 	}()
-
-// 	log.Print("Waiting for messages")
-// 	<-forever
-// }
